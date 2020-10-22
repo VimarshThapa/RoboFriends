f0157ce68079002459a2cd427fa01082e3344994
Vimarsh Thapa <vimarshthapa1030@gmail.com>
2020-10-22 13:52:01 +0530

    initial commit

---
+++ src/components/scroll.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Scroll({children}) {
+    return (
+        <div style={{overflowY: 'scroll', height: '500px'}}>
+            {children}
+        </div>
+    )
+}
+
+export default Scroll;
